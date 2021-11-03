d3cabc63c0da2e931c0fe383e27e67d7f36fee69
Xander Vertegaal <xander.vertegaal@xs4all.nl>
2021-11-03 13:13:51 +0100

    Stylistic tweaks

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <article className="about">
             <h2 className="about-header">About this playlist</h2>
             <p>This web application was created using React in combination with Redux. I see the benefits of working with Redux, but it does take some getting used to the additional boilerplate code. </p>
-            <p> In the future, I might take this Playlist a bit further and incorporate a connection to the Spotify API, so the playlist shows a dropdown menu with suggestions of actual songs/artists/genres in the Spotify database and can display album covers.</p>
+            <p>In the future, I might take this Playlist a bit further and incorporate a connection to the Spotify API, so the playlist shows a dropdown menu with suggestions of actual songs/artists/genres in the Spotify database and can display album covers.</p>
         </article>
     )
 }
